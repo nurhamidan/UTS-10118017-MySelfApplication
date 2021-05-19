40214723464975a4131a49d6326b197da35c178c
nurhamidan <agungnurhamidan@gmail.com>
2021-05-20 06:46:58 +0700

    Menghapus homeViewModel.

--- app/src/main/java/com/nurhamidan/myselfapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/nurhamidan/myselfapplication/ui/home/HomeFragment.java
@@ -34,12 +34,8 @@ public class HomeFragment extends Fragment {
 
     };
 
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         return root;
     }
